bb97806e394f60947ff79f677b8a5291d1d4c196
gdssvpp <pablonogueira16@hotmail.com>
2022-08-15 20:41:53 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/gdssvpp/appreact/AppreactApplication.java
@@ -0,0 +1,13 @@
+package com.gdssvpp.appreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppreactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gdssvpp/appreact/AppreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdssvpp.appreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
