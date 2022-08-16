3dd0cdf05a1aa025e554fab546c82e6f129136b9
gdssvpp <pablonogueira16@hotmail.com>
2022-08-15 21:07:16 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificationButton from "./components/NotificationButton";
+
+
 function App() {
-    return(
+  return (
+    <>
       <h1>Olá Rapaziada</h1>
-    )
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
